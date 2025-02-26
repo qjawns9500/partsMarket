43d34936ee4fb2f748e952c860fa8189f223cb96
qjawns9500 <61936724+qjawns9500@users.noreply.github.com>
2025-02-26 15:20:18 +0900

    24.02.26

---
+++ src/main/java/org/example/partsmarket_user/PartsmarketUserApplication.java
@@ -0,0 +1,13 @@
+package org.example.partsmarket_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartsmarketUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartsmarketUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/partsmarket_user/PartsmarketUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.partsmarket_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartsmarketUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
